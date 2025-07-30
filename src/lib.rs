2065a1a52d2aa30131ba96a1a3d2e50c282d0f60
rustonbsd <rustonbsd@mailfence.com>
2025-07-30 14:37:47 +0200

    refactored p01 to p01_refined

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-pub mod p01;
+pub mod p01_refined;
+pub use p01_refined as p01;
 
 pub fn unix_minute(minute_offset: i64) -> u64 {
     ((chrono::Utc::now().timestamp() as f64 / 60.0f64).floor() as i64 + minute_offset) as u64
